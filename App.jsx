ec27f0e13a249a35befaf2215643d1f96273ef52
Rava Maulana <ravamaulana14@gmail.com>
2023-03-26 18:05:39 +0700

    feat: add DriverCard component

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NewInput from './src/components/NewInput';
+import DriverCard from './src/components/DriverCard';
+// import NewInput from './src/components/NewInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NewInput isBlue={true} isOrange={false} isPassword={false}></NewInput>
+      {/* <NewInput isBlue={true} isOrange={false} isPassword={false}></NewInput> */}
+      <DriverCard></DriverCard>
       <StatusBar style="auto" />
     </View>
   );
