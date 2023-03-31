f69528c73fbefe3aa678f703ca7882915ad079cc
Rava Maulana <ravamaulana14@gmail.com>
2023-03-31 23:58:17 +0700

    feat: add linked input section

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import PickupInputSection from './src/screens/ClientHome/PickupInputSection';
+import LinkedInputSection from './src/screens/ClientHome/LinkedInputSection';
 
 export default function App() {
   return (
     <View>
-      <PickupInputSection/>
+      <LinkedInputSection />
       <StatusBar style="auto" />
     </View>
   );
