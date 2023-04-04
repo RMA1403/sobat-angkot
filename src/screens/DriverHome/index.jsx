2aac36e72e4b2952d9f2d063d2470f92424ade4f
Rava Maulana <ravamaulana14@gmail.com>
2023-04-04 18:28:34 +0700

    feat: add deployment setup

---
+++ src/screens/DriverHome/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const vw = Dimensions.get('window').width;
+
+export default function DriverHome() {
+  const data = {
+    name: 'Mr. John Doe',
+    licenseNumber: 'D 777 KR',
+  };
+}
